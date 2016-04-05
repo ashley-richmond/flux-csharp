e6508870c5d66331a002f873905b355c97bdda95
Ashley Richmond <ashley@lightmare.com.au>
2016-04-06 08:46:48 +1000

    optimised usings

--- Flux/Dispatcher.cs
+++ Flux/Dispatcher.cs
@@ -1,9 +1,6 @@
 ﻿using Flux.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Flux
 {
--- Flux/Interfaces/IPayload.cs
+++ Flux/Interfaces/IPayload.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Flux.Interfaces
+﻿namespace Flux.Interfaces
 {
     public interface IPayload<T>
     {
